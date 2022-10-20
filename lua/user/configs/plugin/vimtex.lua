8c4baf3202aa0442868aba8c8cc85e5030aa299c
XiaotianXu <xxt993018@outlook.com>
2022-10-20 14:17:09 +0800

    don't open the quickfix automatically after latex compiled

--- lua/user/configs/plugin/vimtex.lua
+++ lua/user/configs/plugin/vimtex.lua
@@ -19,7 +19,7 @@ vim.g.tex_flavor='latex'
 -- \ . ':call remote_foreground('''.v:servername.''')^<CR^>^<CR^>\""']])
 
 -- vim.g.tex_comment_nospell = 1
-vim.g.vimtex_quickfix_mode = 1
+vim.g.vimtex_quickfix_mode = 0
 -- vim.g.vimtex_compiler_progname = 'nvr'
 vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
 -- vim.g.vimtex_view_general_options_latexmk = '--reuse-instance'
